f4230273892afc29620ab213315be41aad0d143d
otisjuliusson <4juliuss@informatik.uni-hamburg.de>
2017-02-21 12:14:59 +0100

    added partypeople to title

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+var AlchemyLanguageV1 = require('watson-developer-cloud/alchemy-language/v1');
+
+var alchemy_language = new AlchemyLanguageV1({
+    api_key: 'f2bebbda9a39f53c8a9f92b232ee3238f32b164c'
+});
+
+var params = {
+    text: 'IBM Watson won the Jeopardy television show hosted by Alex Trebek'
+};
+
+alchemy_language.sentiment(params, function (err, response) {
+    if (err)
+        console.log('error:', err);
+    else
+        console.log(JSON.stringify(response, null, 2));
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
