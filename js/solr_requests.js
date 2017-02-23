15dbb95caf0a6dc10731ee55261d5b6d28011e15
Hauke <hauke.dau@googlemail.com>
2017-02-23 17:36:34 +0100

    function to search words in solr and get the amount of the words in the texts

--- js/solr_requests.js
+++ js/solr_requests.js
@@ -12,6 +12,7 @@ function testFunction(){
 
 function solrQuery(){
 // Require module
+    var searchword = "Urteil"
     var SolrNode = require('solr-node');
 
 // Create client
@@ -22,7 +23,7 @@ function solrQuery(){
         protocol: 'http'
     });
 
-    var myStrQuery = 'fl=*,termfreq(_text_,Bund)&indent=on&q=*&wt=json';
+    var myStrQuery = 'fl=*,termfreq(_text_,'+searchword+')&indent=on&q=*&wt=json';
 
 // Search documents using myStrQuery
     client.search(myStrQuery, function (err, result) {
@@ -30,7 +31,12 @@ function solrQuery(){
             console.log(err);
             return;
         }
-        console.log('Response:', result.response);
+        //console.log('Response:', result.response);
+        console.log("gesuchtes Wort: "+searchword);
+        for(i = 0; i < result.response.docs.length; i++){
+
+            console.log("Document Name: "+result.response.docs[i].stream_name[0]+ " Anzahl von "+searchword+": "+ result.response.docs[i]["termfreq(_text_,"+searchword+")"]);
+        }
     });
 
 
