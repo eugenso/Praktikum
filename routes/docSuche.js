3eba6be7eba820d5491227f44d7849a7e7edb9d8
Hauke <hauke.dau@googlemail.com>
2017-03-02 16:58:11 +0100

    find Documents after Document search

--- routes/docSuche.js
+++ routes/docSuche.js
@@ -18,29 +18,48 @@ router.post('/', function(req, res, next) {
 
     function showresult(metaDaten){
         console.log(metaDaten);
+        metaDaten = JSON.parse(metaDaten);
         resultDaten = splitSearchResult(metaDaten);
         res.render('docSuche', { result: resultDaten });
     }
 
 });
 function splitSearchResult(metaDaten){
-    result = {};
-    foundDocuments = [];
-    Document = {};
-    if(metaDaten != undefined){
+    //console.log("split search Result");
+    var result = {};
 
+
+    if(metaDaten == undefined){
+        console.log("not searched yet");
+        result.message = "not searched yet";
     }
     else if(metaDaten.error != undefined ){
-        metaDaten.error = "no Data in searchfield or Error occured";
+        metaDaten.error = "Error search field empty?";
+        console.log("error occured (z.B. no Search Field content) ");
+        result.message = "Fehler, ist das Suchfeld leer? ";
     }
-    else if(metaDaten.response.docs != undefined){
-
+    else if(metaDaten.response.numFound == 0){
+        console.log("no result found");
+        result.message = "Es wurden keine Ergebnisse gefunden";
     }
     else{
-        result.searchword = metaDaten.responseHeader.q;
+        console.log("is here trying");
+        result.message = "nix";
+        result.searchword = metaDaten.responseHeader.params.q.replace("_text_:"," ");
+        result.docs = [];
+
+        for(var i = 0; i <metaDaten.response.docs.length; i++){
+            var foundDocument = {};
+            foundDocument.id = metaDaten.response.docs[i].id;
+            foundDocument.name = returnStringBetween(metaDaten.response.docs[i].id,"data\\",".txt");
+            result.docs.push(foundDocument);
+        }
     }
-
-
+    return result;
+}
+function returnStringBetween (inputString, characterA, characterB){
+    var outputString = inputString.split(characterA).pop().split(characterB).shift();
+    return outputString;
 }
 
 module.exports = router;
