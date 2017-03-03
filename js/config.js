61b3fc12208e9951375f7d8ad802bad44fd4d910
otisjuliusson <4juliuss@informatik.uni-hamburg.de>
2017-03-03 12:41:46 +0100

    colorstuff

--- js/config.js
+++ js/config.js
@@ -2,11 +2,18 @@
  * Created by hauke on 03.03.2017.
  */
 
+//Haukes Key und Model
+
 
 exports.GLOBAL_api_key = 'f2bebbda9a39f53c8a9f92b232ee3238f32b164c';
-exports.GLOBAL_model_id = 'rb:a267d1b0-0fb2-490a-8a30-8c11277be192';
+exports.GLOBAL_model_id = 'b222139d-b256-4640-8397-02e8d3ad58a2';
+
+
+//BrOtis Key und Model
+
 /*
 exports.GLOBAL_api_key ='68ed580532a101015a5e689be50ddc966afa1c7f';
-exports.GLOBAL_model_id ='rb:16d8bb4f-757f-4950-8723-5a69ec98348e';*/
+exports.GLOBAL_model_id ='rb:16d8bb4f-757f-4950-8723-5a69ec98348e';
+*/
 
 exports.solrDataPath = "C:\\\\solr\\\\data\\\\";
